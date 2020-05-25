0c5f4d736749ffbe99ba28fddc3f8b36b085a39c
antarticuno <antarticuno@gmail.com>
2020-05-25 18:59:45 -0400

    config to point to backend

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ const allConfigurations = {
     backendAPI: 'http://localhost:7555',
   },
   production: {
-    backendAPI: 'https://cs4500-sp19-teamrocket.herokuapp.com/api',
+    backendAPI: 'http://localhost:7555',
   },
   test: {
     // TODO: If we had a UAT/Testing environment set up consistently we would point this there
-    backendAPI: 'https://cs4500-sp19-teamrocket.herokuapp.com/api',
+    backendAPI: '',
   },
 };
 
