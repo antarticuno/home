b53df3381a7a9276108e02b7fb1d8b44e0e820e9
antarticuno <antarticuno@gmail.com>
2021-09-02 22:24:28 -0400

    fix About page

--- src/About.js
+++ src/About.js
@@ -7,43 +7,31 @@ import github from './ionicons.designerpack/logo-github.svg';
 
 const About = ({history}) =>
   <div id="about">
-    <div>
-      <div className="bio">
-        <p><img src={face} alt="my-face"/></p>
-        <h3>
-          <a href="https://www.facebook.com/brendan.yang.754"><img className="social-img"
-                                                                   src={fb}
-                                                                   alt="fb"/></a>&nbsp;
-          <a href="https://www.instagram.com/antarticuno/"><img className="social-img"
-                                                                src={insta}/></a>&nbsp;
-          Brendan Yang&nbsp;
-          <a href="https://www.linkedin.com/in/yangbrendan"><img className="social-img"
-                                                                 src={linked}/></a>&nbsp;
-          <a href="https://github.com/antarticuno"><img className="social-img"
-                                                        src={github}/></a>
-        </h3>
-        <p><span>Hi!</span> I am a recent graduate of Northeastern University, from which
-          I
-          earned a
-          combined degree in Biology and Computer Science. During my time at Northeastern
-          I
-          cultivated my love for both research and software development and have a
-          sampling
-          of experience in both. I completed two co-ops, one at a cutting-edge research
-          company, Editas Medicine, and the other at a leading health insurance company,
-          Cigna. I also have some experience in academia, as I volunteered in the Godoy
-          microbiology laboratory.
+    <div className="bio">
+      <img src={face} alt="my-face"/>
+      <h3>
+        <a href="https://www.facebook.com/brendan.yang.754"><img className="social-img"
+                                                                 src={fb}
+                                                                 alt="fb"/></a>
+        <a href="https://www.instagram.com/antarticuno/"><img className="social-img"
+                                                              src={insta}/></a>
+        Brendan Yang
+        <a href="https://www.linkedin.com/in/yangbrendan"><img className="social-img"
+                                                               src={linked}/></a>
+        <a href="https://github.com/antarticuno"><img className="social-img"
+                                                      src={github}/></a>
+      </h3>
+      <div className="description">
+        <p><span>Welcome to my portfolio!</span> I'm an artist and software engineer currently based in the Boston area.
+          Recently, I've spent the past few years developing software for the clinical trial space. It has been incredibly
+          rewarding, especially because of its importance in advancing global health.
         </p>
-        <p>Outside of my academic interests, I love to paint and design things. This
-          leaches
-          into a lot of my other hobbies which include makeup, baking, and web design. I
-          also love to play tabletop board and card games including the increasingly
-          popular
-          game, Magic the Gathering.</p>
-        <p>Feel free to reach me at <i>antarticuno@gmail.com</i> if you have any
-          questions,
-          or would like to contact me about job opportunities/commissions, etc. I look
-          forward to speaking with you!</p>
+        <p>Outside of work, I enjoy drawing and painting, and just generally experimenting with a variety of mediums.
+          I can often be found doodling on my tablet, or scribbling away in my journal. Additionally, I love to take inspiration
+          from board games and video games - which has developed into a healthy obsession for games like Magic the Gathering
+          and Dungeons and Dragons. Feel free to peruse some of my work - I do take commissions from time to time.</p>
+        <p>You can reach me at <em><a href="mailto:antarticuno@gmail.com">antarticuno@gmail.com</a></em> if you have any
+          questions, or would like to contact me about job opportunities/commissions, etc.</p>
       </div>
     </div>
   </div>;
